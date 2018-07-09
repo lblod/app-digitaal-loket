da11d2a94f04cac52b5388f3ba41192491c7cc92
Erika Pauwels <erika.pauwels@gmail.com>
2018-07-09 12:57:15 +0200

    Fix prefix of nmo ontology in repository.lisp

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -30,4 +30,4 @@
 (add-prefix "bbcdr" "http://mu.semte.ch/vocabularies/ext/bbcdr/")
 (add-prefix "validation" "http://mu.semte.ch/vocabularies/validation/")
 (add-prefix "toezicht" "http://mu.semte.ch/vocabularies/ext/supervision/")
-(add-prefix "nmo" "http://oscaf.sourceforge.net/nmo.html#")
+(add-prefix "nmo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#")
