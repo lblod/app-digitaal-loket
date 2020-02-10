5bc8c5f1f1eb40c93de15521566ee5559863d613
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-10 13:43:33 +0100

    added reports for reporting service

---
+++ config/report-service/files/bbcdrReport.js
@@ -0,0 +1,25 @@
+import {generateReportFromData} from '../helpers.js'
+import { querySudo as query } from '@lblod/mu-auth-sudo';
+
+export default {
+  cronPattern: '0 0 * * *',
+  name: 'bbcdrDossiers',
+  execute: async () => {
+    const reportData = {
+      title: 'BBC-DR Dossiers Report',
+      description: 'Number of BBC-DR Dossiers',
+      filePrefix: 'bbcdrDossiers'
+    }
+    console.log('Generate BBC-DR Dossiers Report')
+    const queryString = `
+      SELECT DISTINCT  (COUNT(?uri) AS ?bbcdrReportCount) WHERE {
+        ?uri a <http://mu.semte.ch/vocabularies/ext/bbcdr/Report>
+      }
+    `
+    const queryResponse = await query(queryString)
+    const data = queryResponse.results.bindings.map((row) => ({
+      bbcdrReportCount: row.bbcdrReportCount.value,
+    }))
+    await generateReportFromData(data, ['bbcdrReportCount'], reportData)
+  }
+}
