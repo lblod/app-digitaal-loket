96f364dfa1efd802dd95c4061e961c34a9b3a93e
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-10 13:19:26 +0100

    update dispatcher rules for reports

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -333,6 +333,14 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://adressenregister/"
   end
 
+  #################################################################
+  # Reports
+  #################################################################
+
+  match "/reports/*path" do
+    Proxy.forward conn, path, "http://resource/reports/"
+  end
+
   match _ do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
