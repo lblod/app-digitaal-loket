2645fab727f47c4111adbf79e52194d008336cc3
Erika Pauwels <erika.pauwels@gmail.com>
2020-02-24 17:43:31 +0100

    Replace reporting config file

---
+++ config/reports/index.js
@@ -0,0 +1,18 @@
+import BestuurseenhedenReport from './bestuurseenhedenReport';
+import BerichtencentrumMessagesReport from './berichtencentrumMessages';
+import InzendingenReport from './inzendingenReport';
+import AccountReport from './accountReport';
+import bbcdrReport from './bbcdrReport';
+import sameRrnReport from './sameRrnReport';
+import failedSyncToMft from './failedSyncToMft';
+
+
+export default [
+  BestuurseenhedenReport,
+  BerichtencentrumMessagesReport,
+  InzendingenReport,
+  AccountReport,
+  bbcdrReport,
+  sameRrnReport,
+  failedSyncToMft
+];
