925b7df8ce49b15e7c77b65ebc1654d29318f3f6
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-04-13 11:19:28 +0200

    clean up

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,18 +1,4 @@
 export default [
-  // {
-  //   match: {
-  //     subject: { }
-  //   },
-  //   callback: {
-  //     url: 'http://resource/.mu/delta',
-  //     method: 'POST'
-  //   },
-  //   options: {
-  //     resourceFormat: 'v0.0.1',
-  //     gracePeriod: 250,
-  //     ignoreFromSelf: true
-  //   }
-  // },
   {
     match: {
       subject: { }
