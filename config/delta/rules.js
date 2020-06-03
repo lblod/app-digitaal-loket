4e6e61b5ecff7ea846acdc04cdedf7beecd16230
Claire Lovisa <lovisa.claire@gmail.com>
2020-06-03 10:46:29 +0200

    Add configuration to trigger sync notification on sync error creation

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -197,5 +197,22 @@ export default [
       gracePeriod: 1000,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://purl.org/pav/createdBy'
+      }
+    },
+    callback: {
+      url: 'http://sync-with-kalliope-error-notification/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
   }
 ];
