d2fa8e27a6680c46f35f1e608c00ba5ba60c1e83
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-14 15:55:48 +0200

    No inverse is required here

--- config/resources/master-submissions-domain.lisp
+++ config/resources/master-submissions-domain.lisp
@@ -50,6 +50,8 @@
   :class (s-prefix "ext:Vendor") ; Subclass of foaf:Agent
   :properties `((:name :string ,(s-prefix "foaf:name"))
                 (:key :string ,(s-prefix "muAccount:key")))
+  :has-many `((bestuurseenheid :via ,(s-prefix "muAccount:canActOnBehalfOf")
+                    :as "can-act-on-behalf-of"))
   :resource-base (s-url "http://data.lblod.info/vendors/")
   :features `(include-uri)
   :on-path "vendors")
