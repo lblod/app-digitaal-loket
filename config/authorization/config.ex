c41a572c7280bbbd6d430316d4c1d841554c843b
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-14 15:50:07 +0200

    Remove security liability in mu-auth
    `%AlwaysAccessible{}` should not be the case for graph
    `<http://mu.semte.ch/graphs/automatic-submission>`
    
    This would mean unauthenticated could get all vendor data.

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -205,17 +205,6 @@ defmodule Acl.UserGroups.Config do
                         "http://lblod.data.gift/vocabularies/automatische-melding/FormData"
                       ] } } ] },
 
-      # // AUTOMATIC SUBMISSION
-      %GroupSpec{
-        name: "o-toezicht-vendor-rwf",
-        useage: [:read],
-        access: %AlwaysAccessible{},
-        graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/graphs/automatic-submission",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://mu.semte.ch/vocabularies/ext/Vendor"
-                      ] } } ] },
 
       # // VENDOR MANAGEMENT
       %GroupSpec{
