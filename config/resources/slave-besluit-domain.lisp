faa40b1e64bee90feab724a849bf2baf4b2e6b15
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-20 16:11:29 +0200

    Added inverse bestuurseenheid -> vendors

--- config/resources/slave-besluit-domain.lisp
+++ config/resources/slave-besluit-domain.lisp
@@ -88,17 +88,23 @@
                 (:wil-mail-ontvangen :boolean ,(s-prefix "ext:wilMailOntvangen")) ;;Voorkeur in berichtencentrum
                 (:mail-adres :string ,(s-prefix "ext:mailAdresVoorNotificaties"))
                 (:submission-trial-user :string ,(s-prefix "ext:submissionTrialUser")))
+
   :has-one `((werkingsgebied :via ,(s-prefix "besluit:werkingsgebied")
                              :as "werkingsgebied")
              (werkingsgebied :via ,(s-prefix "ext:inProvincie")
                              :as "provincie")
              (bestuurseenheid-classificatie-code :via ,(s-prefix "besluit:classificatie")
                                                  :as "classificatie"))
+
   :has-many `((contact-punt :via ,(s-prefix "schema:contactPoint")
                             :as "contactinfo")
               (bestuursorgaan :via ,(s-prefix "besluit:bestuurt")
                               :inverse t
-                              :as "bestuursorganen"))
+                              :as "bestuursorganen")
+              (vendor :via ,(s-prefix "muAccount:canActOnBehalfOf")
+                              :inverse t
+                              :as "vendors"))
+
   :resource-base (s-url "http://data.lblod.info/id/bestuurseenheden/")
   :features '(include-uri)
   :on-path "bestuurseenheden"
