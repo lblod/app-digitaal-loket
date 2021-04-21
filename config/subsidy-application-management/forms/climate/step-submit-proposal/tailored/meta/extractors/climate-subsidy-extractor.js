cd20dc9b0c5ba96f45673ae14c173a4bd48918e5
Claire Lovisa <lovisa.claire@gmail.com>
2021-04-21 17:29:54 +0200

    Fix climate data import and extraction

--- config/subsidy-application-management/forms/climate/step-submit-proposal/tailored/meta/extractors/climate-subsidy-extractor.js
+++ config/subsidy-application-management/forms/climate/step-submit-proposal/tailored/meta/extractors/climate-subsidy-extractor.js
@@ -1,48 +1,46 @@
 module.exports = {
   name: 'climate-subsidy-extractor',
   execute: async (store, graphs, lib, form) => {
-    console.log("BONJOUUUUUUUUUUUUUUUUUUUUUUUUUUUR");
     const { $rdf, mu, sudo } = lib;
     const LBLOD_SUBSIDIE = new $rdf.Namespace('http://lblod.data.gift/vocabularies/subsidie/');
     const DBPEDIA = new $rdf.Namespace('http://dbpedia.org/ontology/');
-
-    console.log('ON CALCUUUUUUUUUUUUUUULE')
     const { population, drawingRight } = await getBestuurInfo(form.uri, mu, sudo);
-    console.log(population, drawingRight);
 
     if (population && drawingRight) {
       store.add($rdf.sym(form.uri), DBPEDIA('populationTotal'), population, graphs.additions);
       store.add($rdf.sym(form.uri), LBLOD_SUBSIDIE('drawingRight'), drawingRight, graphs.additions);
     }
-
   }
-
 };
 
 // -------------------------------- INTERNAL LOGIC --------------------------------
 
 async function getBestuurInfo(formUri, mu, sudo) {
   const queryResult = await sudo.querySudo(`
-    PREFIX pav: <http://purl.org/pav/>
-    PREFIX lblodSubsidie: <http://lblod.data.gift/vocabularies/subsidie/>
     PREFIX dbpedia: <http://dbpedia.org/ontology/>
-    PREFIX m8g: <http://data.europa.eu/m8g/>
     PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX lblodSubsidie: <http://lblod.data.gift/vocabularies/subsidie/>
+    PREFIX m8g: <http://data.europa.eu/m8g/>
     PREFIX subsidie: <http://data.vlaanderen.be/ns/subsidie#>
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 
-    SELECT ?population ?drawingRight WHERE {
-      GRAPH ?g {
-        ${mu.sparqlEscapeUri(formUri)} pav:createdBy ?bestuurseenheid .
-
+    SELECT DISTINCT ?population ?drawingRight where {
+      GRAPH ?h {
+        ${mu.sparqlEscapeUri(formUri)} <http://purl.org/dc/terms/isPartOf> ?flowStep .
+        ?subsidyAmount
+          subsidie:maximumbedrag ?drawingRight ;
+          dct:requires ?requirementGroup .
         ?requirementGroup
           m8g:hasCriterionRequirement ?criterionRequirementBestuurseenheid ;
           m8g:hasCriterionRequirement ?criterionRequirementPopulation .
-
-        ?criterionRequirementBestuurseenheid lblodSubsidie:isSatisfiableBy ?bestuurseenheid .
-
-        ?criterionRequirementPopulation dbpedia:populationTotal ?population .
-        ?subsidyAmount dct:requires ?requirementGroup ;
-          subsidie:maximumbedrag ?drawingRight .
+        ?criterionRequirementPopulation a m8g:CriterionRequirement ;
+          dbpedia:populationTotal ?population .
+      }
+      GRAPH ?g {
+        ?flowStep <http://purl.org/dc/terms/references> ?proceduralStep .
+        ?criterion <http://purl.org/dc/terms/isPartOf> ?proceduralStep ;
+          m8g:fulfilledByRequirementGroup ?requirementGroup .
       }
     }
   `);
