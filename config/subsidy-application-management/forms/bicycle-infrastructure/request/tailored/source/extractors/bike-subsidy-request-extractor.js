99eca76458cbbbaa479b8485fdd1af5f179b4114
Claire Lovisa <lovisa.claire@gmail.com>
2021-08-13 10:20:53 +0200

    Load dossiernummer from step 1

--- config/subsidy-application-management/forms/bicycle-infrastructure/request/tailored/source/extractors/bike-subsidy-request-extractor.js
+++ config/subsidy-application-management/forms/bicycle-infrastructure/request/tailored/source/extractors/bike-subsidy-request-extractor.js
@@ -10,6 +10,7 @@ module.exports = {
       const DCT = new $rdf.Namespace('http://purl.org/dc/terms/');
       const RDF = new $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
       const EXT = new $rdf.Namespace('http://mu.semte.ch/vocabularies/ext/');
+      const NIE = new $rdf.Namespace('http://www.semanticdesktop.org/ontologies/2007/01/19/nie#');
       const URI_TEMPLATE = 'http://data.lblod.info/form-data/nodes/';
 
       const previousForm = await getPreviousForm(form.uri, mu, sudo);
@@ -20,6 +21,7 @@ module.exports = {
         const contactPointUri = new $rdf.NamedNode(URI_TEMPLATE + mu.uuid());
 
         store.add($rdf.sym(form.uri), LBLOD_SUBSIDIE('projectName'), info.projectName, graphs.additions);
+        store.add($rdf.sym(form.uri), NIE('identifier'), info.dossiernummer, graphs.additions);
         store.add($rdf.sym(form.uri), SCHEMA('contactPoint'), $rdf.sym(contactPointUri), graphs.additions);
         store.add($rdf.sym(contactPointUri), FOAF('firstName'), info.firstName, graphs.additions);
         store.add($rdf.sym(contactPointUri), FOAF('familyName'), info.familyName, graphs.additions);
@@ -98,13 +100,15 @@ async function getPreviousStepInfo(formUri, mu, sudo) {
     PREFIX schema: <http://schema.org/>
     PREFIX dct: <http://purl.org/dc/terms/>
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
-    SELECT DISTINCT ?projectName ?decisionUpload ?estimatedCostTable ?objectiveTable ?contactPoint
+    SELECT DISTINCT ?projectName ?dossiernummer ?decisionUpload ?estimatedCostTable ?objectiveTable ?contactPoint
                     ?firstName ?familyName ?email ?telephone ?validEstimatedCostTable ?validObjectiveTable
     WHERE {
       GRAPH ?g {
         ${mu.sparqlEscapeUri(formUri)}
           lblodSubsidie:projectName ?projectName ;
+          nie:identifier ?dossiernummer ;
           lblodSubsidie:decisionUpload ?decisionUpload ;
           bicycleInfrastructure:estimatedCostTable ?estimatedCostTable ;
           bicycleInfrastructure:objectiveTable ?objectiveTable ;
@@ -124,6 +128,7 @@ async function getPreviousStepInfo(formUri, mu, sudo) {
 
   if (queryResult.results.bindings.length) {
     const projectName = queryResult.results.bindings[0].projectName.value;
+    const dossiernummer = queryResult.results.bindings[0].dossiernummer.value;
     const decisionUpload = queryResult.results.bindings[0].decisionUpload.value;
     const estimatedCostTable = queryResult.results.bindings[0].estimatedCostTable.value;
     const validEstimatedCostTable = queryResult.results.bindings[0].validEstimatedCostTable.value;
@@ -136,6 +141,7 @@ async function getPreviousStepInfo(formUri, mu, sudo) {
     const telephone = queryResult.results.bindings[0].telephone.value;
     return {
       projectName,
+      dossiernummer,
       decisionUpload,
       estimatedCostTable,
       validEstimatedCostTable,
