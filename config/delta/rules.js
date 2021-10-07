44e993b6ea9cf209013683bbcec0d5df7ae977c5
Dries Hugaerts <dries.hugaerts@live.be>
2021-10-07 15:51:06 +0200

    introduced the loket-error-alert-service + bumped automatische-meldingen-service to v0.10.0-beta that provides error logging of errors into the database/store.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -341,5 +341,26 @@ export default [
       ignoreFromSelf: true,
       optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value:'http://open-services.net/ns/core#Error'
+      }
+    },
+    callback: {
+      url: 'http://error-alert/delta',
+      method:'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
   }
 ];
