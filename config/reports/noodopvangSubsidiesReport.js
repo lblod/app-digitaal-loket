778e14ed1fc965356d907b1c98f77346dc1b0b59
Claire Lovisa <lovisa.claire@gmail.com>
2021-10-21 15:21:28 +0200

    Less assumptions: noodopvangSubsidiesReport

--- config/reports/noodopvangSubsidiesReport.js
+++ config/reports/noodopvangSubsidiesReport.js
@@ -37,57 +37,57 @@ export default {
           transactie:isInstantieVan <http://lblod.data.gift/concepts/1df4b56a-3ccd-450d-93dc-317fda1ada38> ;
           adms:status <http://lblod.data.gift/concepts/2ea29fbf-6d46-4f08-9343-879282a9f484>.
 
-       OPTIONAL {
-          ?subsidiemaatregelConsumptie cpsv:follows ?applicationFlow ;
-          dct:source ?applicationForm ;
-          dct:modified ?aanvraagdatum ;
-          m8g:hasParticipation ?participation ;
-          ext:lastModifiedBy ?modifiedBy ;
-          dct:creator ?createdBy .
-
-        ?applicationFlow xkos:belongsTo ?measureOfferSeries .
-
-        ?measureOfferSeries dct:title ?reeksLabel ;
-          mobiliteit:periode/m8g:startTime ?reeksStart ;
-          mobiliteit:periode/m8g:endTime ?reeksEnd .
-
-        ?bestuur m8g:playsRole ?participation ;
-          skos:prefLabel ?bestuurseenheid .
-
-        ?applicationForm
-          schema:bankAccount/schema:identifier ?accountNumber ;
-          schema:contactPoint ?contactPoint ;
-          lblodSubsidie:uniqueChildrenNumberForWholePeriod ?aantalUniekeKinderen  ;
-          lblodSubsidie:daysOfChildcareForWholePeriod ?aantalKalenderdagen ;
-          lblodSubsidie:applicationFormTable ?formTable .
-
-        ?contactPoint foaf:firstName ?contactFirstName ;
-          foaf:familyName ?contactLastName ;
-          schema:email ?contactEmail ;
-          schema:telephone ?contactTelephone .
-
-        ?formTable ext:applicationFormEntry ?formEntry .
-
-        ?formEntry ext:actorName ?naamOrganisator ;
-          ext:numberChildrenForFullDay ?aantalKinderenVoorAlleVolleDagen ;
-          ext:numberChildrenForHalfDay ?aantalKinderenVoorAlleHalveDagen ;
-          ext:numberChildrenPerInfrastructure ?aantalKinderenPerInfrastructuur  .
-
-        ?createdBy foaf:firstName ?createdByFirstName ;
-          foaf:familyName ?createdByLastName .
-
-        BIND(CONCAT(?createdByFirstName, " ", ?createdByLastName) as ?createdByName)
-
-        ?modifiedBy foaf:firstName ?modifiedByFirstName ;
-          foaf:familyName ?modifiedByLastName .
-
-        BIND(CONCAT(?modifiedByFirstName, " ", ?modifiedByLastName) as ?modifiedByName)
-
         OPTIONAL {
-          ?applicationForm lblodSubsidie:totalAmount ?amount .
+          ?subsidiemaatregelConsumptie cpsv:follows ?applicationFlow ;
+            dct:source ?applicationForm ;
+            dct:modified ?aanvraagdatum ;
+            m8g:hasParticipation ?participation ;
+            ext:lastModifiedBy ?modifiedBy ;
+            dct:creator ?createdBy .
+
+          ?applicationFlow xkos:belongsTo ?measureOfferSeries .
+
+          ?measureOfferSeries dct:title ?reeksLabel ;
+            mobiliteit:periode/m8g:startTime ?reeksStart ;
+            mobiliteit:periode/m8g:endTime ?reeksEnd .
+
+          ?bestuur m8g:playsRole ?participation ;
+            skos:prefLabel ?bestuurseenheid .
+
+          ?applicationForm
+            schema:bankAccount/schema:identifier ?accountNumber ;
+            schema:contactPoint ?contactPoint ;
+            lblodSubsidie:uniqueChildrenNumberForWholePeriod ?aantalUniekeKinderen  ;
+            lblodSubsidie:daysOfChildcareForWholePeriod ?aantalKalenderdagen ;
+            lblodSubsidie:applicationFormTable ?formTable .
+
+          ?contactPoint foaf:firstName ?contactFirstName ;
+            foaf:familyName ?contactLastName ;
+            schema:email ?contactEmail ;
+            schema:telephone ?contactTelephone .
+
+          ?formTable ext:applicationFormEntry ?formEntry .
+
+          ?formEntry ext:actorName ?naamOrganisator ;
+            ext:numberChildrenForFullDay ?aantalKinderenVoorAlleVolleDagen ;
+            ext:numberChildrenForHalfDay ?aantalKinderenVoorAlleHalveDagen ;
+            ext:numberChildrenPerInfrastructure ?aantalKinderenPerInfrastructuur  .
+
+          ?createdBy foaf:firstName ?createdByFirstName ;
+            foaf:familyName ?createdByLastName .
+
+          BIND(CONCAT(?createdByFirstName, " ", ?createdByLastName) as ?createdByName)
+
+          ?modifiedBy foaf:firstName ?modifiedByFirstName ;
+            foaf:familyName ?modifiedByLastName .
+
+          BIND(CONCAT(?modifiedByFirstName, " ", ?modifiedByLastName) as ?modifiedByName)
+
+          OPTIONAL {
+            ?applicationForm lblodSubsidie:totalAmount ?amount .
+          }
+          BIND(IF(BOUND(?amount), ?amount, xsd:float(0)) as ?totaalBedrag)
         }
-        BIND(IF(BOUND(?amount), ?amount, xsd:float(0)) as ?totaalBedrag)
-       }
       }
       ORDER BY DESC(?aanvraagdatum)
     `;
