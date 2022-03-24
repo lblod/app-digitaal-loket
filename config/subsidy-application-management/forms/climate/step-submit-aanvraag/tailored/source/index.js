f8fab73be5ab23ec083293cd883bd50cdd3e22ab
felix <ruiz.felixruiz@gmail.com>
2022-03-24 17:59:38 +0100

    Simplify index.js extractor

--- config/subsidy-application-management/forms/climate/step-submit-aanvraag/tailored/source/index.js
+++ config/subsidy-application-management/forms/climate/step-submit-aanvraag/tailored/source/index.js
@@ -1,54 +1,47 @@
 const contactInfoExtractor = require('./extractors/contact-info-extractor');
 
-/**
- * NOTE: order of execution bound to position in the array
- */
+const extractor = {
+  name: 'climate-subsidy/opvolgmoment/first-step-as-source',
+  execute
+};
 
-firstFormExtractor = (extractors) =>
-  Object.assign(
-    {
-      name: 'climate-subsidy/opvolgmoment/first-step-as-source',
-      execute: async (store, graphs, lib, form) => {
-        const {mu, sudo} = lib;
+async function execute( store, graphs, lib, form ){
+    const source = await findFormFromStep1(lib, form);
 
-        const { results } = await sudo.querySudo(`
-         PREFIX dct: <http://purl.org/dc/terms/>
-         PREFIX xkos: <http://rdf-vocabulary.ddialliance.org/xkos#>
-
-         SELECT DISTINCT ?firstForm where {
-          GRAPH ?g {
-            ${mu.sparqlEscapeUri(form.uri)} dct:isPartOf ?currentStep .
-            ?smc dct:source ${mu.sparqlEscapeUri(form.uri)} .
-            ?firstForm dct:isPartOf ?step1 .
-            ?smc dct:source ?firstForm .
-          }
-          GRAPH ?h {
-            ?currentStep xkos:previous ?step2 .
-            ?step2 xkos:previous ?step1.
-          }
-        }`);
-
-        if (!results.bindings.length)
-          throw `Failed to find a preciding form for <${form.uri}>, are you sure this is part of a flow?`;
-
-        const source = results.bindings[0].firstForm.value;
-        for (let extractor of extractors) {
-          try {
-            await extractor.execute(
-                store,
-                graphs,
-                lib,
-                form,
-                {uri: source});
-          } catch (e) {
-            console.warn(
-                `Failed to execute inner extractor for ${extractor.name}`);
-            console.log(e);
-          }
-        }
+    for (let extractor of [ contactInfoExtractor ]) {
+      try {
+        await extractor.execute(store, graphs, lib, form, { uri: source });
+      } catch (e) {
+        console.warn(`Failed to execute inner extractor for ${extractor.name}`);
+        console.log(e);
       }
     }
-  )
-module.exports = [
-  firstFormExtractor([contactInfoExtractor]),
-]
+}
+
+async function findFormFromStep1( { mu, sudo }, form ) {
+  const queryStr = `
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX xkos: <http://rdf-vocabulary.ddialliance.org/xkos#>
+
+    SELECT DISTINCT ?firstForm where {
+     GRAPH ?g {
+       ${mu.sparqlEscapeUri(form.uri)} dct:isPartOf ?currentStep .
+       ?smc dct:source ${mu.sparqlEscapeUri(form.uri)} .
+       ?firstForm dct:isPartOf ?step1 .
+       ?smc dct:source ?firstForm .
+     }
+     GRAPH ?h {
+       ?currentStep xkos:previous ?step2 .
+       ?step2 xkos:previous ?step1.
+     }
+   }`;
+
+  const { results } = await sudo.querySudo(queryStr);
+
+  if (!results.bindings.length)
+    throw `Failed to find a preciding form for <${form.uri}>, are you sure this is part of a flow?`;
+
+  return results.bindings[0].firstForm.value;
+}
+
+module.exports = [ extractor ];
