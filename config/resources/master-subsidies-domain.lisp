ce7d833fe348af044a6eb924e366de7a4749f022
felix <ruiz.felixruiz@gmail.com>
2022-05-25 18:42:52 +0200

    Refactor participation: we want to re-use API as much as possible
    Eventually we will need to remove the relation
    participating-bestuurseenheid
    
    and the attribute role.
    
    But a breaking change, so ends up on a TODO

--- config/resources/master-subsidies-domain.lisp
+++ config/resources/master-subsidies-domain.lisp
@@ -173,10 +173,16 @@
 (define-resource participation ()
   :class (s-prefix "m8g:Participation")
   :properties `((:description :string ,(s-prefix "dct:description"))
-                (:role :url ,(s-prefix "m8g:role")))
+                (:role :url ,(s-prefix "m8g:role"))
+                (:identifier :string ,(s-prefix "dct:identifier")))
   :has-one `((bestuurseenheid :via ,(s-prefix "m8g:playsRole")
                             :inverse t
                             :as "participating-bestuurseenheid"))
+  :has-many `((agents :via ,(s-prefix "m8g:playsRole")
+                             :inverse t
+                             :as "participating-agents")
+              (roles :via ,(s-prefix "m8g:role")
+                             :as "roles"))
   :resource-base (s-url "http://data.lblod.info/id/participations/")
   :features '(include-uri)
   :on-path "participations")
