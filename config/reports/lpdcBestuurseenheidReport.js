2a9d91d8dd6a4685f0dc40688136c66699ea4feb
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-31 11:13:12 +0200

    lpdc-bestuurseenheid-report basic info

--- config/reports/lpdcBestuurseenheidReport.js
+++ config/reports/lpdcBestuurseenheidReport.js
@@ -3,11 +3,19 @@ import { querySudo as query } from '@lblod/mu-auth-sudo';
 
 export default {
   cronPattern: '',
-  name: '',
+  name: 'lpdcBestuursenheid',
   execute: async () =>{
     const reportData = {
+      title: 'LPDC bestuursenheid report',
+      description: 'LPDC bestuursenheid report',
     };
+    console.log('Generating LPDC Bestuursenheid Report');
     const queryString  = `
+      PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>  
+      PREFIX lpdcExt: <https://productencatalogus.data.vlaanderen.be/ns/ipdc-lpdc#>
+      PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+      PREFIX cpsv: <http://purl.org/vocab/cpsv#>
     `;
     const queryResponse = await query(queryString);
     const bindings = queryResponse.results.bindings;
