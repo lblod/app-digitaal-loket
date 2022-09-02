c0f1eac5486451b6498a579ecb4c18b131f25b54
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-09-02 11:51:15 +0200

    lpdc bestuurseenheid report add bestuurseenheid type

--- config/reports/lpdcBestuurseenheidReport.js
+++ config/reports/lpdcBestuurseenheidReport.js
@@ -17,9 +17,11 @@ export default {
       PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
       PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
       PREFIX cpsv: <http://purl.org/vocab/cpsv#>
-      SELECT ?uriBestuurseenheid ?naam ?uriPublicService ?title ?modified ?status WHERE {
+      SELECT ?uriBestuurseenheid ?naam ?typeUri ?type ?uriPublicService ?title ?modified ?status WHERE {
             ?uriBestuurseenheid a besluit:Bestuurseenheid;
-                  skos:prefLabel ?naam.
+                  skos:prefLabel ?naam;
+                  besluit:classificatie ?typeUri.
+            ?typeUri skos:prefLabel ?type.
 
             ?uriPublicService a cpsv:PublicService;
                   adms:status ?status;
