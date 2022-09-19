78e1fbf00d434b067524b2ff776ebd196018c84c
Claire Lovisa <lovisa.claire@gmail.com>
2022-09-19 12:30:20 +0200

    Fix config to match prepare-submissions updates

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -141,53 +141,16 @@ export default [
   },
   {
     match: {
-      // anything
-    },
-    callback: {
-      url: 'http://prepare-submissions-for-export/delta',
-      method: 'POST'
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: [
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
-                        ]
-    }
-  },
-  {
-    match: {
-      // anything
-    },
-    callback: {
-      url: 'http://prepare-worship-submissions-for-export/delta',
-      method: 'POST'
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: [
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
-                        ]
-    }
-  },
-  {
-    match: {
       predicate: {
         type: 'uri',
-        value: 'http://www.w3.org/ns/adms#status'
+        value: 'http://mu.semte.ch/vocabularies/ext/formSubmissionStatus' // Status of flattened form data
       },
       object: {
         type: 'uri',
-        value: 'http://lblod.data.gift/automatische-melding-statuses/successful-sent'
-      }
-    },
+        value: 'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c' // Sent
+      }    },
     callback: {
-      url: 'http://toezicht-flattened-form-data-generator/delta',
+      url: 'http://prepare-submissions-for-export/delta',
       method: 'POST'
     },
     options: {
