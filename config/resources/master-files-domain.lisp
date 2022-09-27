1be8799cc316fa93c51d185dbd600af769ec7322
benjay10 <ben.behaeghe@redpencil.io>
2022-09-27 02:02:48 +0200

    Change file model nfo:fileCreated to dct:created
    Change all references of nfo:fileCreated to dct:created.
    Due to a small documentation confusion this property has not been
    updated correctly at the time.

--- config/resources/master-files-domain.lisp
+++ config/resources/master-files-domain.lisp
@@ -11,7 +11,7 @@
                 (:format :string ,(s-prefix "dct:format"))
                 (:size :number ,(s-prefix "nfo:fileSize"))
                 (:extension :string ,(s-prefix "dbpedia:fileExtension"))
-                (:created :datetime ,(s-prefix "nfo:fileCreated")))
+                (:created :datetime ,(s-prefix "dct:created")))
   :has-one `((file :via ,(s-prefix "nie:dataSource")
                    :inverse t
                    :as "download")
