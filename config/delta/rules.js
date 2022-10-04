587ff3c4e91bfed51e0a882d8b47fa93deb9822a
felix <ruiz.felixruiz@gmail.com>
2022-10-04 17:16:53 +0200

    Remove old delta-rule

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -141,24 +141,6 @@ export default [
   },
   {
     match: {
-      // anything
-    },
-    callback: {
-      url: 'http://prepare-submissions-for-export/delta',
-      method: 'POST'
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-      optOutMuScopeIds: [
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
-                        ]
-    }
-  },
-  {
-    match: {
       predicate: {
         type: 'uri',
         value: 'http://www.w3.org/ns/adms#status'
