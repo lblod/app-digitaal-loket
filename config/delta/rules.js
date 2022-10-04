06a293fc96081c7a59ec74ab3f8d201a636f180d
felix <ruiz.felixruiz@gmail.com>
2022-10-04 17:17:32 +0200

    added new delta rule

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -193,6 +193,30 @@ export default [
     match: {
       predicate: {
         type: 'uri',
+        value: 'http://mu.semte.ch/vocabularies/ext/formSubmissionStatus' // Status of flattened form data
+      },
+      object: {
+        type: 'uri',
+        value: 'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c' // Sent
+      }    },
+    callback: {
+      url: 'http://prepare-submissions-for-export/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+                        ]
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
         value: 'http://purl.org/pav/createdBy'
       },
       object: {
