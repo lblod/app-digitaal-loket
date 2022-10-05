6fce4b06d7aa85b6175d1b65b2df1f63e2397e2d
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-10-05 09:30:25 +0200

    add first level delta rule for lpdc publisher

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -18,6 +18,28 @@ export default [
     match: {
       predicate: {
         type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://purl.org/vocab/cpsv#PublicService'
+      }
+    },
+    callback: {
+      url: 'http://lpdc-publish/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
         value: 'http://www.w3.org/ns/adms#status'
       },
       object: {
