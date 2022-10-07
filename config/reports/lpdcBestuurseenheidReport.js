d860919da6d87afd01d4fe4ff3acd37932837886
Fangio <fangiodoom01@gmail.com>
2022-10-07 12:08:24 +0200

    Updated report generation of lpdc to once a day at 6AM

--- config/reports/lpdcBestuurseenheidReport.js
+++ config/reports/lpdcBestuurseenheidReport.js
@@ -2,7 +2,7 @@ import {generateReportFromData} from '../helpers.js';
 import { querySudo as query } from '@lblod/mu-auth-sudo';
 
 export default {
-  cronPattern: '*/15 * * * *',
+  cronPattern: '0 0 6 * * *',
   name: 'lpdcBestuurseenheidReport',
   execute: async () =>{
     const reportData = {
