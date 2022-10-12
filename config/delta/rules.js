e33bfdca4e7ddd2e51894a0ebe94da4fcc450b2b
benjay10 <ben.behaeghe@redpencil.io>
2022-10-12 23:19:15 +0200

    Change the monitored status to CONCEPT
    Instead of monitoring for public services becoming sent, we monitor for
    them becoming concept. The lpdc-publish service will remove the
    publication status if it has already been published before to allow for
    republishing.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -22,7 +22,7 @@ export default [
       },
       object: {
         type: 'uri',
-        value: 'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c'
+        value: 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd'
       }
     },
     callback: {
