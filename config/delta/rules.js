d5200d090a6323c30f3832f723272946eae9dc9d
felix <ruiz.felixruiz@gmail.com>
2022-10-17 14:50:04 +0200

    extended delta-rule

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -217,6 +217,30 @@ export default [
     match: {
       predicate: {
         type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status' // Status of remote-data object coming later
+      },
+      object: {
+        type: 'uri',
+        value: 'http://lblod.data.gift/file-download-statuses/success' // cached
+      }    },
+    callback: {
+      url: 'http://prepare-submissions-for-export/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+                        ]
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
         value: 'http://purl.org/pav/createdBy'
       },
       object: {
