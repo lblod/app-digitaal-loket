00e2b7781355067504448a82848e22a08de1bd6c
felix <ruiz.felixruiz@gmail.com>
2022-11-30 16:05:54 +0100

    added extra delta rule

--- config/delta/lpdc-publish.js
+++ config/delta/lpdc-publish.js
@@ -20,5 +20,27 @@ export default [
       ignoreFromSelf: true,
       optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value: 'https://www.w3.org/ns/activitystreams#Tombstone'
+      }
+    },
+    callback: {
+      url: 'http://lpdc-publish/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync" ]
+    }
   }
 ];
