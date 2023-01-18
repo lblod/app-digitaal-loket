b8d6a2ce57c5c58eacec84743e25180eb4c35c59
David <david.journo.31@gmail.com>
2023-01-18 13:02:41 +0100

    Adding the bestuurseenheid hasEditRight flag
    This flag is true when bestuurseenheiden has module access to eredienst-bedienarenbeheer and eredienst-mandatenbeheer.

--- config/resources/organisation.lisp
+++ config/resources/organisation.lisp
@@ -11,7 +11,8 @@
                 (:alternatieve-naam :string-set ,(s-prefix "skos:altLabel"))
                 (:wil-mail-ontvangen :boolean ,(s-prefix "ext:wilMailOntvangen")) ;;Voorkeur in berichtencentrum
                 (:mail-adres :string ,(s-prefix "ext:mailAdresVoorNotificaties"))
-                (:is-trial-user :boolean ,(s-prefix "ext:isTrailUser")))
+                (:is-trial-user :boolean ,(s-prefix "ext:isTrailUser"))
+                (:has-edit-right :boolean ,(s-prefix "ext:hasEditRight")))
 
   :has-one `((werkingsgebied :via ,(s-prefix "besluit:werkingsgebied")
                              :as "werkingsgebied")
