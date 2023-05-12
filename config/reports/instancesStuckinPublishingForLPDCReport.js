31669f60b53dc00831ed0a30e6492c3d5ffa9fbb
Riad Abdallah <riad.abdallah@hotmail.com>
2023-05-12 12:22:20 +0200

    Add extra fields for easier debuggability

--- config/reports/instancesStuckinPublishingForLPDCReport.js
+++ config/reports/instancesStuckinPublishingForLPDCReport.js
@@ -18,13 +18,31 @@ export default {
     PREFIX schema:  <http://schema.org/>
     PREFIX adms:    <http://www.w3.org/ns/adms#>
     PREFIX as:      <https://www.w3.org/ns/activitystreams#>
-    
-    SELECT DISTINCT ?publicService WHERE {
+    PREFIX dct:     <http://purl.org/dc/terms/>
+    PREFIX pav:     <http://purl.org/pav/>
+    PREFIX rdfs-ns: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+
+    SELECT DISTINCT ?publicService ?type ?title ?bestuurseenheidLabel ?classificatieLabel WHERE {
       {
         GRAPH ?graph {
           ?publicService a cpsv:PublicService ;
-            adms:status <http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c> .
+            rdfs-ns:type  ?type ;
+            dct:title     ?title ;
+            pav:createdBy ?bestuurseenheid ;
+            adms:status   <http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c> .
+
+            FILTER(LANG(?title) = "nl")
         }
+
+        GRAPH ?g {
+          ?bestuurseenheid skos:prefLabel ?bestuurseenheidLabel ;
+            besluit:classificatie ?classificatie .
+
+          ?classificatie skos:prefLabel ?classificatieLabel .
+        }
+
         FILTER NOT EXISTS {
           ?publicService schema:publication <http://lblod.data.gift/concepts/3369bb10-1962-11ed-b07c-132292303e92> .
         }
@@ -32,15 +50,40 @@ export default {
       UNION {
         GRAPH ?graph {
           ?publicService a cpsv:PublicService ;
+            rdfs-ns:type        ?type ;
+            dct:title           ?title ;
+            pav:createdBy       ?bestuurseenheid ;
             adms:status         <http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c> ;
             schema:publication  <http://lblod.data.gift/concepts/a7d01120-6f93-11ed-bcb8-a144c50c46d7> .
+
+            FILTER(LANG(?title) = "nl")
+        }
+
+        GRAPH ?g {
+          ?bestuurseenheid skos:prefLabel ?bestuurseenheidLabel ;
+            besluit:classificatie ?classificatie .
+
+          ?classificatie skos:prefLabel ?classificatieLabel .
         }
       }
       UNION {
         GRAPH ?graph {
           ?publicService a as:Tombstone ;
+            rdfs-ns:type        ?type ;
             as:formerType       cpsv:PublicService ;
             schema:publication  <http://lblod.data.gift/concepts/a7d01120-6f93-11ed-bcb8-a144c50c46d7> .
+
+          # Graphs have a consistent structure.
+          # Ex: http://mu.semte.ch/graphs/organizations/8620c62b9e51d2275c98cb724ce4b6784b432db8e1e0376ac70cbda098ea0d0a/LoketLB-LPDCGebruiker
+          BIND(SUBSTR(STR(?graph), 41, STRLEN(?graph) - 62) AS ?bestuurseenheidUUID) .
+        }
+
+        GRAPH ?g {
+          ?bestuurseenheid ?p ?bestuurseenheidUUID .
+          ?bestuurseenheid skos:prefLabel ?bestuurseenheidLabel ;
+            besluit:classificatie ?classificatie .
+
+          ?classificatie skos:prefLabel ?classificatieLabel .
         }
       }
     }
