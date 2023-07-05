af47afd28ddffd3a1939e48db5585135d391c48a
benjay10 <ben.behaeghe@redpencil.io>
2023-07-05 18:15:52 +0200

    Added some comments on helper functions in reports

--- config/reports/worship/queries.js
+++ config/reports/worship/queries.js
@@ -1,3 +1,13 @@
+/**
+ * Produces a query to get all the data for the given collection of subjects.
+ *
+ * @public
+ * @function
+ * @param {Array(String)} subjectURIs - An array of strings representing the
+ * subjects you want all the data of. This means all the `?s ?p ?o` where `?s`
+ * is one of the subjects in the array.
+ * @returns {String} The query to execute.
+ */
 export function dataForSubjects(subjectURIs) {
   const subjectValues = subjectURIs.map((uri) => `<${uri}>`).join(' ');
   return `
@@ -12,6 +22,18 @@ SELECT ?g ?s ?p ?o WHERE {
   `;
 }
 
+/**
+ * Produces a query that returns all RolBedienaren that where harvested. The
+ * provenance is checked by the existence of a triple `?bedienaar
+ * <http://www.w3.org/ns/prov#wasGeneratedBy>
+ * <http://lblod.data.gift/id/app/lblod-harvesting> .`. Triples are also
+ * filtered so that the ones from the temporary insert and delete graphs are
+ * ignored.
+ *
+ * @public
+ * @function
+ * @returns {String} The SPARQL query to execute.
+ */
 export function allBedienaren() {
   return `
 SELECT DISTINCT ?bedienaar WHERE {
@@ -26,6 +48,18 @@ SELECT DISTINCT ?bedienaar WHERE {
   `;
 }
 
+/**
+ * Produces a query that returns all EredienstMandatarissen that where
+ * harvested. The provenance is checked by the existence of a triple
+ * `?mandataris <http://www.w3.org/ns/prov#wasGeneratedBy>
+ * <http://lblod.data.gift/id/app/lblod-harvesting> .`. Triples are also
+ * filtered so that the ones from the temporary insert and delete graphs are
+ * ignored.
+ *
+ * @public
+ * @function
+ * @returns {String} The SPARQL query to execute.
+ */
 export function allMandatarissen() {
   return `
 SELECT DISTINCT ?mandataris WHERE {
@@ -40,6 +74,22 @@ SELECT DISTINCT ?mandataris WHERE {
   `;
 }
 
+/**
+ * Produces a query that collects subjects related to a collection of given
+ * subjects via a given predicate. The collected subjects will also be of the
+ * given type. E.g. you have a list of books and you want to collect all the
+ * authors: supply that list, a predicate such as `ont:writtenBy`, and
+ * `ont:Author` as arguments to this function to get all the subjects of the
+ * Author type that have written this books. There is no relation between the
+ * books and the authors, just a new unordered collection of authors.
+ *
+ * @public
+ * @function
+ * @param {Array(String)} domainURIs - This collection contains subjects where you want related subjects of.
+ * @param {String} predicate - Represents the full RDF predicate to get related objects that are new RDF entities.
+ * @param {String} type - Represents the `rdf:type` of the related subject.
+ * @returns {String} The SPARQL query to execute.
+ */
 export function subjectToRange(domainURIs, predicate, type) {
   const subjectValues = domainURIs.map((uri) => `<${uri}>`).join(' ');
   return `
@@ -53,6 +103,16 @@ SELECT DISTINCT ?range {
   `;
 }
 
+/**
+ * Produces a query to get all the triples from a graph. No pagination, just
+ * everything at once.
+ *
+ * @public
+ * @function
+ * @param {String} graphURI - Represents the URI of the graph you want the
+ * contents of.
+ * @returns {String} The SPARQL query to execute.
+ */
 export function allFromGraph(graphURI) {
   return `
 SELECT DISTINCT ?s ?p ?o WHERE {
