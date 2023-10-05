36ef55ffac77e93f104dd0e7822b0c35f4e4be12
benjay10 <ben.behaeghe@redpencil.io>
2023-10-05 18:24:29 +0200

    Add subject types for berichten in mu-auth
    To acces them through the Vendor API.

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -548,6 +548,10 @@ defmodule Acl.UserGroups.Config do
                         "http://rdf.myexperiment.org/ontologies/base/Submission",
                         "http://mu.semte.ch/vocabularies/ext/SubmissionDocument",
                         "http://lblod.data.gift/vocabularies/automatische-melding/FormData",
+                        "http://schema.org/Conversation",
+                        "http://schema.org/Message",
+                        "http://vocab.deri.ie/cogs#Job"
+                        '
                       ] } } ] },
 
       # // LOKETADMIN
