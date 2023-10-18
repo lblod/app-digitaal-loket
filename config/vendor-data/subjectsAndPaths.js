729864442645e0d98d724ec917408e799edaeeb7
benjay10 <ben.behaeghe@redpencil.io>
2023-10-18 03:38:55 +0200

    New config for the new VDDS

--- config/vendor-data/subjectsAndPaths.js
+++ config/vendor-data/subjectsAndPaths.js
@@ -1,22 +1,136 @@
+const HOSTNAME = 'http://localhost';
+
 export const subjects = [
+  //For Submissions via Automatic Submission Flow
+  //TODO could use some improvements (maybe add some more things like a Job)
   {
     type: 'http://rdf.myexperiment.org/ontologies/base/Submission',
+    trigger: `
+      ?subject a <http://rdf.myexperiment.org/ontologies/base/Submission> .
+    `,
     path: `
       ?subject
         pav:createdBy ?organisation ;
         pav:providedBy ?vendor .
     `,
+    remove: {
+      delete: `
+        ?subject ?p ?o .
+      `,
+      where: `
+        ?subject ?p ?o .
+      `,
+    },
+    copy: {
+      insert: `
+        ?subject ?p ?o .
+      `,
+      where: `
+        ?subject ?p ?o .
+      `,
+    },
   },
+  //For the Berichten Melding via berichten-melding-service
   {
     type: 'http://vocab.deri.ie/cogs#Job',
+    trigger: `
+      ?subject
+        <http://www.w3.org/ns/adms#status>
+          <http://redpencil.data.gift/id/concept/JobStatus/success> ;
+        <http://redpencil.data.gift/vocabularies/tasks/operation>
+          <http://lblod.data.gift/id/jobs/concept/JobOperation/harvestBericht> .
+    `,
     path: `
       ?subject
         <http://purl.org/pav/providedBy> ?vendor ;
         <http://schema.org/sender> ?organisation .
     `,
+    remove: {
+      delete: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+      where: `
+        ?subject
+          <http://purl.org/dc/terms/subject> ?bericht .
+        ?bericht
+          a <http://schema.org/Message> .
+        ?conversatie
+          <http://schema.org/hasPart> ?bericht ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    },
+    copy: {
+      insert: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+
+        ?bijlage
+          <http://mu.semte.ch/vocabularies/ext/downloadLink>
+            ?bijlageDownloadLink .
+      `,
+      where: `
+        ?subject
+          <http://purl.org/dc/terms/subject> ?bericht .
+        ?bericht
+          a <http://schema.org/Message> .
+        ?conversatie
+          <http://schema.org/hasPart> ?bericht ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://mu.semte.ch/vocabularies/core/uuid> ?bijlageUUID .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        BIND (CONCAT("${HOSTNAME}/files/", STR(?bijlageUUID), "/download") AS ?bijlageDownloadLink)
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    },
   },
+  //For Berichten en Conversaties synced from Kaliope
   {
     type: 'http://schema.org/Message',
+    trigger: `
+      ?subject
+        <http://www.w3.org/ns/adms#status>
+          <http://data.lblod.info/id/status/berichtencentrum/sync-with-kalliope/delivered/confirmed> .
+    `,
     path: `
       GRAPH ?h {
         ?organisation a <http://data.vlaanderen.be/ns/besluit#Bestuurseenheid> .
@@ -29,17 +143,82 @@ export const subjects = [
         ?organisation ?p ?o .
       }
     `,
-    // Better when counting on the existence of Jobs and Tasks for the Message:
-    //path: `
-    //  ?ic
-    //    <http://purl.org/dc/terms/subject> ?subject ;
-    //    a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#DataContainer> ;
-    //    <http://purl.org/pav/providedBy> ?vendor ;
-    //    <http://schema.org/sender> ?organisation .
-    //`,
+    remove: {
+      delete: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+      where: `
+        ?conversatie
+          <http://schema.org/hasPart> ?subject ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    },
+    copy: {
+      insert: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+
+        ?bijlage
+          <http://mu.semte.ch/vocabularies/ext/downloadLink>
+            ?bijlageDownloadLink .
+      `,
+      where: `
+        ?conversatie
+          <http://schema.org/hasPart> ?subject ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        BIND (CONCAT("${HOSTNAME}/files/", STR(?bijlageUUID), "/download") AS ?bijlageDownloadLink)
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    }
   },
+  //For Berichten saved in Loket
+  //TODO: Trigger is not yet working like this
   {
-    type: 'http://schema.org/Conversation',
+    type: 'http://schema.org/Message',
+    trigger: `
+      ?subject <???> <???> .
+    `,
     path: `
       GRAPH ?h {
         ?organisation a <http://data.vlaanderen.be/ns/besluit#Bestuurseenheid> .
@@ -52,5 +231,73 @@ export const subjects = [
         ?organisation ?p ?o .
       }
     `,
+    remove: {
+      delete: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+      where: `
+        ?conversatie
+          <http://schema.org/hasPart> ?subject ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    },
+    copy: {
+      insert: `
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+
+        ?bijlage
+          <http://mu.semte.ch/vocabularies/ext/downloadLink>
+            ?bijlageDownloadLink .
+      `,
+      where: `
+        ?conversatie
+          <http://schema.org/hasPart> ?subject ;
+          a <http://schema.org/Conversation> ;
+          <http://schema.org/hasPart> ?alleberichten .
+        ?alleberichten
+          a <http://schema.org/Message> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#hasPart>
+            ?bijlage .
+        ?bijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+        ?physicalBijlage
+          a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> ;
+          <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#dataSource>
+            ?bijlage .
+
+        BIND (CONCAT("${HOSTNAME}/files/", STR(?bijlageUUID), "/download") AS ?bijlageDownloadLink)
+
+        ?subject ?p ?o .
+        ?conversatie ?pc ?co .
+        ?alleberichten ?pa ?ca .
+        ?bijlage ?pb ?ob .
+        ?physicalBijlage ?pp ?op .
+      `,
+    }
   },
 ];
