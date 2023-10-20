127199fd2113227542ace439f11cd739dfe89c41
benjay10 <ben.behaeghe@redpencil.io>
2023-10-20 03:34:14 +0200

    Added more filters on the delta-notifier
    The added rule about adms:status was originally there before. The
    intention was te replace it, but it turns out very useful.

--- config/delta/vendor-data-distribution.js
+++ config/delta/vendor-data-distribution.js
@@ -1,4 +1,25 @@
 export default [
+  //Submissions via Automatic Submission Flow
+  //Berichten via Berichten Melding
+  //Berichten via Berichten Sync Kaliope
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+    },
+    callback: {
+      url: 'http://vendor-data-distribution/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
+  //Berichten from Loket
   {
     match: {
       predicate: {
