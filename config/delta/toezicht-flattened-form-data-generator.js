74bb923e541b2b9cf9346a52df75f0ef206d892c
Riad Abdallah <riad.abdallah@hotmail.com>
2025-03-17 00:29:14 +0100

    Opt out of VDDS deltas for `resource` and `toezicht-flattened`

--- config/delta/toezicht-flattened-form-data-generator.js
+++ config/delta/toezicht-flattened-form-data-generator.js
@@ -45,7 +45,8 @@ export default [
       ignoreFromSelf: true,
       optOutMuScopeIds: [
                           "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
-                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance"
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance",
+                          "http://redpencil.data.gift/id/concept/muScope/deltas/vendor-data"
       ]
     }
   },
