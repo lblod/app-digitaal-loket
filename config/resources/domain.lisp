b5b4677fa1d1d81b6c04dcdc9e46480c30da54b0
felix <ruiz.felixruiz@gmail.com>
2025-11-26 10:13:14 +0100

    Disable all caches from resource (and fix the rammifications)

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,9 +1,9 @@
 (in-package :mu-cl-resources)
 
-(defparameter *cache-count-queries* nil)
-(defparameter *supply-cache-headers-p* t
-  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
-(setf *cache-model-properties-p* t)
+(defparameter *cache-count-queries* nil) ;;Temporary disabled because sparql-parser is generating too much deltas
+(defparameter *supply-cache-headers-p* nil
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.") ;;Temporary disabled because sparql-parser is generating too much deltas
+(setf *cache-model-properties-p* nil) ;;Temporary disabled because sparql-parser is generating too much deltas
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
